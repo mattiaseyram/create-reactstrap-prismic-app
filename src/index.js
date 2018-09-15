eb24e8c596274389cd965f52d8de558f58c6b4d2
Mattias Eyram <mattiaseyram@gmail.com>
2018-09-15 11:34:20 -0700

    Took away react seo

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { HashRouter } from "react-router-dom";
+import ReactSEO from 'react-seo';
 
 //CSS Files
-import './Styles/index.css'; 
+import './Styles/index.css';
+
 
 ReactDOM.render(
     <HashRouter>
         <App />
     </HashRouter>, document.getElementById('root'));
 registerServiceWorker();
+
+
